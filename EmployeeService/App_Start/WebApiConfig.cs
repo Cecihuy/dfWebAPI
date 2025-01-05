70e41b31f26ef45cb77f71136ef2215d60ebc857
VisualStudio2022 <vs2022@mail.it>
2025-01-05 12:01:42 +0700

    6 ASP NET Web API MediaTypeFormatter

--- EmployeeService/App_Start/WebApiConfig.cs
+++ EmployeeService/App_Start/WebApiConfig.cs
@@ -2,9 +2,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace EmployeeService {
+  public class CustomJsonFormatter : JsonMediaTypeFormatter {
+    public CustomJsonFormatter() {
+      this.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+    }
+    public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType) {
+      base.SetDefaultContentHeaders(type, headers, mediaType);
+      headers.ContentType = new MediaTypeHeaderValue("application/json");
+    }
+  }
   public static class WebApiConfig {
     public static void Register(HttpConfiguration config) {
       // Web API configuration and services
@@ -17,8 +28,12 @@ namespace EmployeeService {
           routeTemplate: "api/{controller}/{id}",
           defaults: new { id = RouteParameter.Optional }
       );
-      config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
-      config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+      //config.Formatters.Remove(config.Formatters.JsonFormatter);
+      //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+      config.Formatters.Add(new CustomJsonFormatter());
+
+      //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+      //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
     }
   }
 }
