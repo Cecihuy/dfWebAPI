3ad7eb758dcfb510004dd5260217107e6b136c4c
VisualStudio2022 <vs2022@mail.it>
2025-01-05 10:31:49 +0700

    4 ASP NET Web API and SQL Server

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace EmployeeService.Controllers {
+  public class EmployeesController : ApiController {
+    public IEnumerable<Employee> Get() {
+      using(EmployeeDbEntities entities = new EmployeeDbEntities()) {
+        return entities.Employees.ToList();
+      }
+    }
+    public Employee Get(int id) {
+      using(EmployeeDbEntities entities = new EmployeeDbEntities()) {
+        return entities.Employees.FirstOrDefault(e => e.Id == id);
+      }
+    }
+  }
+}
