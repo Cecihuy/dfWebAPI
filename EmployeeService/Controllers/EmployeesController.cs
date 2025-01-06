c5f8ddd72c413d6371d3108dd8fea86a634f8931
VisualStudio2022 <vs2022@mail.it>
2025-01-06 11:35:42 +0700

    7 Implementing post method in ASP NET Web API

--- EmployeeService/Controllers/EmployeesController.cs
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -13,9 +13,29 @@ namespace EmployeeService.Controllers {
         return entities.Employees.ToList();
       }
     }
-    public Employee Get(int id) {
+    public HttpResponseMessage Get(int id) {
       using(EmployeeDbEntities entities = new EmployeeDbEntities()) {
-        return entities.Employees.FirstOrDefault(e => e.Id == id);
+        var entity = entities.Employees.FirstOrDefault(e => e.Id == id);
+        if(entity != null) {
+          return Request.CreateResponse(HttpStatusCode.OK, entity);
+        } else {
+          return Request.CreateErrorResponse(
+            HttpStatusCode.NotFound, "Employee with id = " + id.ToString() + " not found"
+          );
+        }
+      }
+    }
+    public HttpResponseMessage Post([FromBody]Employee employee) {
+      try {
+        using(EmployeeDbEntities entities = new EmployeeDbEntities()) {
+          entities.Employees.Add(employee);
+          entities.SaveChanges();
+          var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+          message.Headers.Location = new Uri(Request.RequestUri + employee.Id.ToString());
+          return message;
+        }
+      } catch(Exception ex) {
+        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
       }
     }
   }
