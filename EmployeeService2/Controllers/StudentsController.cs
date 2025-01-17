b2977da655919878f427d3ad414153c57bf9c6a7
VisualStudio2022 <vs2022@mail.it>
2025-01-17 16:00:24 +0700

    30 Attribute routing in ASP NET Web API 2

---
+++ EmployeeService2/Controllers/StudentsController.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeService2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService2.Controllers {
+  public class StudentsController : ApiController {
+    static List<Student> students = new List<Student>() {
+      new Student{Id = 1, Name = "Tome"},
+      new Student{Id = 2, Name = "Sam"},
+      new Student{Id = 3, Name = "John"}
+    };
+    public IEnumerable<Student> Get() {
+      return students;
+    }
+    public Student Get(int id) {
+      return students.FirstOrDefault(s => s.Id == id);
+    }
+    [Route("api/students/{id}/courses")]
+    public IEnumerable<string> GetStudentCourses(int id) {
+      if(id == 1) {
+        return new List<string>() { "C#", "ASP.NET", "SQL SERVER" };
+      } else if (id == 2){
+        return new List<string>() { "ASP.NET WEB API", "C#", "SQL SERVER" };
+      } else {
+        return new List<string>() { "Bootstrap", "jQuery", "AngularJs" };
+      }
+    }
+  }
+}
---
+++ EmployeeService2/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeService2.Models {
+  public class Student {
+    public int Id { get; set; }
+    public string Name { get; set; }
+  }
+}
