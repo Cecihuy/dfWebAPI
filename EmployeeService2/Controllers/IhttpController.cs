60ebdecf45fb45e15d2cb06d6adf85dd264aa537
VisualStudio2022 <vs2022@mail.it>
2025-01-20 09:28:48 +0700

    34 IHttpActionResult vs HttpResponseMessage

---
+++ EmployeeService2/Controllers/IhttpController.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeService2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService2.Controllers{
+  public class IhttpController : ApiController
+  {
+    static List<Student> students = new List<Student>(){
+      new Student{Id=1,Name="Tome"},
+      new Student{Id=2,Name="Sam"},
+      new Student{Id=3,Name="John"}
+    };
+    //public HttpResponseMessage Get() {
+    //  return Request.CreateResponse(HttpStatusCode.OK, students);
+    //}
+    public IHttpActionResult Get() {
+      return Ok(students);
+    }
+    public IHttpActionResult Get(int id) {
+      var student = students.FirstOrDefault(s => s.Id == id);
+      if(student == null) {
+        return Content(HttpStatusCode.NotFound, "student with id " + id + " notfound");
+      }
+      return Ok(student);
+    }
+  }    
+}
