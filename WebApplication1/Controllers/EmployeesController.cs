199823365cadbf496bc0573c624dd27a9f029cd9
VisualStudio2022 <vs2022@mail.it>
2025-01-23 21:20:26 +0700

    5 ASP NET Web API Content Negotiation (.net8)

--- WebApplication1/Controllers/EmployeesController.cs
+++ WebApplication1/Controllers/EmployeesController.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System.Collections;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using WebApplication1.DataAccess;
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,21 +1,35 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
 using WebApplication1.DataAccess;
 
 namespace WebApplication1 {
   public class Program {
     public static void Main(string[] args) {
+      /* ========== services ========== */
       var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddControllers();
-        builder.Services.AddDbContext<AppDbContext>(options => 
-          options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection"))
-        );
+      builder.Services.AddControllers();
+      //  .AddNewtonsoftJson(options => {
+      //    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+      //    options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+      //    options.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+      //  });
+      builder.Services.Configure<JsonOptions>(options => {
+        //options.JsonSerializerOptions.PropertyNamingPolicy = null;
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;        
+        options.JsonSerializerOptions.WriteIndented = true;
+      });
+      builder.Services.AddDbContext<AppDbContext>(options => 
+        options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection"))
+      );      
+      /* ========== pipelines ========== */
       var app = builder.Build();
-        app.UseAuthorization();
-        app.MapControllers();
-        app.Run();
+      app.UseAuthorization();
+      app.MapControllers();
+      app.Run();
     }
   }
 }
