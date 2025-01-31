e8b3a022ca369d526da9f9d80140815ae70048dd
VisualStudio2022 <vs2022@mail.it>
2025-01-31 14:25:18 +0700

    7 Implementing post method in ASP NET Web API

--- WebApplication1/Controllers/EmployeesController.cs
+++ WebApplication1/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using WebApplication1.DataAccess;
@@ -11,13 +12,35 @@ namespace WebApplication1.Controllers {
     public EmployeesController(AppDbContext appDb) {
       this.appDb = appDb;
     }
+    [HttpGet]
     [Route("api/[controller]")]
     public IEnumerable<Employee> Get() {
       return appDb.Employees.ToList();
     }
-    [Route("api/[controller]/{id}")]
-    public Employee GetById(int id) {
-      return appDb.Employees.FirstOrDefault(e => e.Id == id);
+    [HttpGet]
+    [Route("api/[controller]/{id}", Name = "getLinkWithId")]
+    public IActionResult GetById(int id) {
+      Employee employee = appDb.Employees.FirstOrDefault(e => e.Id == id);
+      if(employee != null) {
+        return Ok(employee);
+      } else {
+        return NotFound($"Employee with id = {id} not found");
+      }
+    } 
+    [HttpPost]
+    [Route("api/[controller]")]
+    public IActionResult Post([FromBody] Employee employee) {
+      try {
+        appDb.Employees.Add(employee);
+        appDb.SaveChanges();
+        return Created(
+          new Uri(
+            Url.Link("getLinkWithId", new { id = employee.Id }).ToLower()
+          ), employee
+        );
+      } catch(Exception ex) {
+        return BadRequest(ex);
+      }
     }
   }
 }
